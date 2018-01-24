a42b0dc21d07e15f2a2911d9e6f0b2346cc46c19
Sam Chung <chungsam@outlook.com>
2018-01-23 21:53:30 -0800

    first commit

---
+++ seedData.js
@@ -0,0 +1,80 @@
+const activities =
+    [
+        {
+            title: "Seniors Golf Tournament",
+            date: new Date(2018, 2, 6),
+            startTime: new Date(2018, 2, 6, 8, 30),
+            endTime: new Date(2018, 2, 6, 10, 30)
+        },
+        {
+            title: "Community General Assembly Meeting",
+            date: new Date(2018, 2, 7),
+            startTime: new Date(2018, 2, 7, 18, 30),
+            endTime: new Date(2018, 2, 7, 20, 00)
+        },
+        {
+            title: "Youth Bowling Tournament",
+            date:new Date(2018, 2, 9),
+            startTime: new Date(2018, 2, 9, 17, 30),
+            endTime: new Date(2018, 2, 9, 19, 15)
+        },
+        {
+            title: "Ladies Cooking Lesson",
+            date:new Date(2018, 2, 9),
+            startTime: new Date(2018, 2, 9, 19, 15),
+            endTime: new Date(2018, 2, 9, 20, 00)
+        },
+        {
+            title: "Youth Craft Lessons",
+            date: new Date(2018, 2, 10),
+            startTime: new Date(2018, 2, 10, 8, 30),
+            endTime: new Date(2018, 2, 10, 10, 30)
+        },
+        {
+            title: "Adult Choir Practice",
+            date: new Date(2018, 2, 10),
+            startTime: new Date(2018, 2, 10, 10, 30),
+            endTime: new Date(2018, 2, 10, 12, 00)
+        },
+        {
+            title: "Lunch",
+            date: new Date(2018, 2, 10, 12, 00),
+            startTime: new Date(2018, 2, 10, 12, 00),
+            endTime: new Date(2018, 2, 10, 13, 30)
+        },
+        {
+            title: "Pancake Breakfast",
+            date: new Date(2018, 2, 11), 
+            startTime: new Date(2018, 2, 11, 7, 30),
+            endTime: new Date(2018, 2, 11, 8, 30)
+        }, {
+            title: "Swimming lessons for the youth",
+            date: new Date(2018, 2, 11),
+            startTime: new Date(2018, 2, 11, 8, 30),
+            endTime: new Date(2018, 2, 11, 10, 30)
+        },
+        {
+            title: "Swimming exercise for parents",
+            date: new Date(2018, 2, 11),
+            startTime: new Date(2018, 2, 11, 8, 30),
+            endTime: new Date(2018, 2, 11, 10, 30)
+        },
+        {
+            title: "Bingo tournament",
+            date: new Date(2018, 2, 11),
+            startTime: new Date(2018, 2, 11, 10, 30),
+            endTime: new Date(2018, 2, 11, 12, 00)
+        },
+        {
+            title: "Lunch",
+            date: new Date(2018, 2, 11),
+            startTime: new Date(2018, 2, 11, 12, 00),
+            endTime: new Date(2018, 2, 11, 13, 00)
+        },
+        {
+            title: "Garage sale",
+            date: new Date(2018, 2, 11),
+            startTime: new Date(2018, 2, 11, 13, 00),
+            endTime: new Date(2018, 2, 11, 15, 00)
+        }
+    ];
